3c7017300d3a3b8a4a08e12479096967f5008d6b
Tóth Krisztián Gyula <t.krisztian95@gmail.com>
2019-06-23 16:36:21 +0200

    Local storage + check dom change

--- content.js
+++ content.js
@@ -1,19 +1,19 @@
-var titleCards = document.querySelectorAll('[id^="title-"]');
-
 var apiKey = '';
 
-const fetchIMDBValue = async (title) => {
-    console.log('Fetch imdb rating of: "' + title + '"');
+const fetchImdbRating = async (title) => {
+    console.log('Fetching imdb rating of: "' + title + '"');
     const response = await fetch('https://www.omdbapi.com/?apikey=' + apiKey + '&t=' + title);
     const myJson = await response.json(); //extract JSON from the http response
-    return myJson.imdbRating;
+    var imdbRating = myJson.imdbRating;
+    console.log('Fetched imdb rating of: "' + title + ' is ' + imdbRating);
+    return imdbRating;
 }
 
 function delay(elem, callback) {
     var timeout = null;
     elem.onmouseover = function () {
-        // Set timeout to be a timer which will invoke callback after 1s
-        timeout = setTimeout(callback(elem), 5000);
+        // Set timeout to be a timer which will invoke callback after 3s
+        timeout = setTimeout(callback(elem), 3000);
     };
 
     elem.onmouseout = function () {
@@ -22,24 +22,52 @@ function delay(elem, callback) {
     }
 };
 
-for (var i = 0, l = titleCards.length; i < l; i++) {
-    var elem = titleCards[i].firstChild;
-    //elem.insertAdjacentHTML('afterbegin', innerHTML);
-
-    delay(elem, function (elem) {
-        var title = elem.firstElementChild.innerText;
-        console.log(title);
-        fetchIMDBValue(title).then(function (result) {
-            var innerHTML = '<p style="background-color: red; \
-            height: 15px; \
-            width: 75px; \
-            z-index: 999999; \
-            margin-top: 0px; \
-            position: absolute; \
-            top:0; right:0;">'+ "IMDb rating: " + result + '</p>';
-            console.log(result);
-            elem.insertAdjacentHTML('afterbegin', innerHTML);
-        });
-
-    });
+function getInnerHTML(rating) {
+    var innerHTML = '<p style="background-color: red; \
+    z-index: 2; \
+    margin-top: 0px; \
+    position: absolute; \
+    top:0; right:0;">'+ "IMDb rating: " + rating + '</p>';
+    return innerHTML;
 }
+
+function getTitleFromCard(elem) {
+    var title = elem.firstElementChild.innerText;
+    console.log(title);
+    return title;
+}
+
+function addRatingToTitleCardElement(elem) {
+    if (!elem.hasAttribute("imdb-rating")) {
+        var title = getTitleFromCard(elem);
+        var ratingInLocalStorage = localStorage.getItem(title);
+        if (!ratingInLocalStorage) {
+            fetchImdbRating(title).then(function (imdbRating) {
+                localStorage.setItem(title, imdbRating);
+                elem.insertAdjacentHTML('beforeend', getInnerHTML(imdbRating));
+                elem.setAttribute("imdb-rating", imdbRating);
+            });
+        } else {
+            elem.insertAdjacentHTML('beforeend', getInnerHTML(ratingInLocalStorage));
+            elem.setAttribute("imdb-rating", ratingInLocalStorage);
+        }
+    }
+}
+
+function checkDOMChange()
+{
+    // check for any new element being inserted here,
+    // or a particular node being modified
+    var titleCards = document.querySelectorAll('[id^="title-"]');
+    for (var i = 0, l = titleCards.length; i < l; i++) {
+        var elem = titleCards[i].firstChild;
+        delay(elem, addRatingToTitleCardElement);
+    }
+    // call the function again after 100 milliseconds
+    setTimeout( checkDOMChange, 100 );
+}
+
+checkDOMChange();
+
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# TODO
+
+- [x] Use Chrome's local storage, cache already fetched rating of titles
+- [ ] Reasearch other IMDb rating provider API
+- [ ] GUI to porivide user's API key
