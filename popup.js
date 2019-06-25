5f442617a132949c5c3fdc84de4d0885a33e7809
Tóth Krisztián Gyula <t.krisztian95@gmail.com>
2019-06-25 23:40:27 +0200

    Popup html style

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ function saveApiKey() {
     var jsonfile = {};
     jsonfile[LOCAL_STORAGE_API_KEY] = apikey;
     chrome.storage.sync.set(jsonfile);
+    document.getElementById("display_message").style.display= '';
     console.log("OMDb api key updated: " + JSON.stringify(jsonfile));
 }
 
@@ -12,3 +13,9 @@ document.getElementById("btnSetApiKey").addEventListener("click", saveApiKey);
 chrome.storage.sync.get(LOCAL_STORAGE_API_KEY, function (result) {
     document.getElementById('apikey').value = result[LOCAL_STORAGE_API_KEY];
 });
+
+window.addEventListener('click',function(e){
+    if(e.target.href!==undefined){
+      chrome.tabs.create({url:e.target.href})
+    }
+  })
