b37cd664f32a1b1fc3de8d36131f10cf13b2c896
d-x90 <work.dhorvath@gmail.com>
2020-12-16 10:14:46 +0100

    Codestyle improvements

--- js/content.js
+++ js/content.js
@@ -1,115 +1,134 @@
-const LOCAL_STORAGE_RATINGS = "imdb_ratings";
-const LOCAL_STORAGE_API_KEY = "omdb_apikey";
+const LOCAL_STORAGE_RATINGS = 'imdb_ratings';
+const LOCAL_STORAGE_API_KEY = 'omdb_apikey';
 
-var apiKey = "";
+let apiKey = '';
 
-chrome.storage.sync.get(LOCAL_STORAGE_API_KEY, function (result) {
+chrome.storage.sync.get(LOCAL_STORAGE_API_KEY, (result) => {
     console.log(result);
     apiKey = result[LOCAL_STORAGE_API_KEY];
-    console.log("Api key loaded:" + apiKey)
+    console.log('Api key loaded:' + apiKey);
 });
 
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-    for (var key in changes) {
-        var storageChange = changes[key];
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    for (let key in changes) {
+        const storageChange = changes[key];
         if (key === LOCAL_STORAGE_API_KEY) {
-            console.log("Api key updated");
+            console.log('Api key updated');
             apiKey = storageChange.newValue;
         }
-        console.log('Storage key "%s" in namespace "%s" changed. ' +
-            'Old value was "%s", new value is "%s".',
-            key,
-            namespace,
-            storageChange.oldValue,
-            storageChange.newValue);
+        console.log(
+            `Storage key "${key}" in namespace "${namespace}" changed. 
+            Old value was "${storageChange.oldValue}", new value is "${storageChange.newValue}".`
+        );
     }
 });
 
 const fetchImdbRating = async (title) => {
-    console.log('Fetching imdb rating of: "' + title + '"');
-    const response = await fetch(encodeURI('https://www.omdbapi.com/?apikey=' + apiKey + '&t=' + title));
-    const myJson = await response.json(); //extract JSON from the http response
+    console.log(`Fetching imdb rating of: "${title}"`);
+    let imdbRating = '';
+    try {
+        const response = await fetch(
+            encodeURI(`https://www.omdbapi.com/?apikey=${apiKey}&t=${title}`)
+        );
+        const myJson = await response.json(); //extract JSON from the http response
 
-    var imdbRating = myJson.imdbRating;
+        imdbRating = myJson.imdbRating;
 
-    if (myJson["Error"] && myJson["Error"] === "Movie not found!") {
-        console.log("Movie not found:" + title);
-        imdbRating = "Not found";
-    } else {
-        console.log('Fetched imdb rating of: "' + title + ' is ' + imdbRating);
+        if (myJson['Error'] && myJson['Error'] === 'Movie not found!') {
+            console.log(`Movie not found: ${title}`);
+            imdbRating = 'Not found';
+        } else {
+            console.log(`Fetched imdb rating of: "${title} is ${imdbRating}`);
+        }
+    } catch (e) {
+        console.log(`Something went wrong while fetching rating of ${title}`);
+        imdbRating = 'Something went wrong.';
     }
+
     return imdbRating;
-}
+};
 
 function delay(elem, callback) {
-    var timeout = null;
-    elem.onmouseover = function () {
+    let timeout = null;
+    elem.onmouseover = () => {
         // Set timeout to be a timer which will invoke callback after 3s
         timeout = setTimeout(callback(elem), 3000);
     };
 
-    elem.onmouseout = function () {
+    elem.onmouseout = () => {
         // Clear any timers set to timeout
         clearTimeout(timeout);
-    }
-};
+    };
+}
 
 function getInnerHTML(rating) {
-    var innerHTML = '<p style="background-color: red; \
-    z-index: 2; \
-    margin-top: 0px; \
-    position: absolute; \
-    top:0; right:0;">'+ "IMDb rating: " + rating + '</p>';
+    const innerHTML = `
+    <p style="background-color: red;
+        z-index: 2;
+        margin-top: 0px;
+        position: absolute;
+        top:0; right:0;"
+    >IMDb rating: ${rating}</p>`;
+
     return innerHTML;
 }
 
 function getTitleFromCard(elem) {
-    var title = elem.firstElementChild.innerText;
+    const title = elem.firstElementChild.innerText;
     console.log(title);
     return title;
 }
 
 function addRatingToTitleCardElement(elem) {
-    if (!elem.hasAttribute("imdb-rating")) {
-        var title = getTitleFromCard(elem);
-        var ratingInLocalStorage = getRatingFromLocalStorage(title);
+    if (!elem.hasAttribute('imdb-rating')) {
+        const title = getTitleFromCard(elem);
+        const ratingInLocalStorage = getRatingFromLocalStorage(title);
         if (!ratingInLocalStorage) {
-            fetchImdbRating(title).then(function (imdbRating) {
+            fetchImdbRating(title).then((imdbRating) => {
                 storeRatingInLocalStorage(title, imdbRating);
                 elem.insertAdjacentHTML('beforeend', getInnerHTML(imdbRating));
-                elem.setAttribute("imdb-rating", imdbRating);
+                elem.setAttribute('imdb-rating', imdbRating);
             });
         } else {
-            elem.insertAdjacentHTML('beforeend', getInnerHTML(ratingInLocalStorage));
-            elem.setAttribute("imdb-rating", ratingInLocalStorage);
+            elem.insertAdjacentHTML(
+                'beforeend',
+                getInnerHTML(ratingInLocalStorage)
+            );
+            elem.setAttribute('imdb-rating', ratingInLocalStorage);
         }
     }
 }
 
 function storeRatingInLocalStorage(title, rating) {
-    if (title && rating && rating !== "Not found") {
-        var ratingsInStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_RATINGS));
+    if (title && rating && rating !== 'Not found') {
+        let ratingsInStorage = JSON.parse(
+            localStorage.getItem(LOCAL_STORAGE_RATINGS)
+        );
         if (!ratingsInStorage) {
             ratingsInStorage = {};
         }
         ratingsInStorage[title] = rating;
-        localStorage.setItem(LOCAL_STORAGE_RATINGS, JSON.stringify(ratingsInStorage));
+        localStorage.setItem(
+            LOCAL_STORAGE_RATINGS,
+            JSON.stringify(ratingsInStorage)
+        );
     }
 }
 
 function getRatingFromLocalStorage(title) {
-    var ratingsInStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_RATINGS));
-    var rating = ratingsInStorage && ratingsInStorage[title];
+    const ratingsInStorage = JSON.parse(
+        localStorage.getItem(LOCAL_STORAGE_RATINGS)
+    );
+    const rating = ratingsInStorage && ratingsInStorage[title];
     return rating;
 }
-
 
 function checkDOMChange() {
     // check for any new element being inserted here,
     // or a particular node being modified
-    var titleCards = document.querySelectorAll('[id^="title-"]');
+    const titleCards = document.querySelectorAll('[id^="title-"]');
     for (var i = 0, l = titleCards.length; i < l; i++) {
-        var elem = titleCards[i].firstChild;
+        const elem = titleCards[i].firstChild;
         delay(elem, addRatingToTitleCardElement);
     }
     // call the function again after 100 milliseconds
@@ -117,6 +136,3 @@ function checkDOMChange() {
 }
 
 checkDOMChange();
-
-
-
--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,21 @@
-const LOCAL_STORAGE_API_KEY = "omdb_apikey";
+const LOCAL_STORAGE_API_KEY = 'omdb_apikey';
 
 function saveApiKey() {
-    var apikey = document.getElementById('apikey').value;
-    var jsonfile = {};
-    jsonfile[LOCAL_STORAGE_API_KEY] = apikey;
-    chrome.storage.sync.set(jsonfile);
-    document.getElementById("display_message").style.display= '';
-    console.log("OMDb api key updated: " + JSON.stringify(jsonfile));
+    const apiKey = document.getElementById('apikey').value;
+    const jsonFile = {};
+    jsonFile[LOCAL_STORAGE_API_KEY] = apiKey;
+    chrome.storage.sync.set(jsonFile);
+    document.getElementById('display_message').style.display = '';
+    console.log(`OMDb api key updated: ${JSON.stringify(jsonFile)}`);
 }
 
-document.getElementById("btnSetApiKey").addEventListener("click", saveApiKey);
-chrome.storage.sync.get(LOCAL_STORAGE_API_KEY, function (result) {
+document.getElementById('btnSetApiKey').addEventListener('click', saveApiKey);
+chrome.storage.sync.get(LOCAL_STORAGE_API_KEY, (result) => {
     document.getElementById('apikey').value = result[LOCAL_STORAGE_API_KEY];
 });
 
-window.addEventListener('click',function(e){
-    if(e.target.href!==undefined){
-      chrome.tabs.create({url:e.target.href})
+window.addEventListener('click', (e) => {
+    if (e.target.href !== undefined) {
+        chrome.tabs.create({ url: e.target.href });
     }
-  })
+});
